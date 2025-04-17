4667abee68d6b84b1978adc82a4a1eb0f86cb00a
FrostWillmott <121764288+FrostWillmott@users.noreply.github.com>
2025-04-17 15:01:21 +0300

    first commit

--- items_exchange/urls.py
+++ items_exchange/urls.py
@@ -1,8 +1,8 @@
-"""
-URL configuration for items_exchange project.
+"""URL configuration for items_exchange project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
+
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,10 +13,11 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -39,12 +40,14 @@ schema_view = get_schema_view(
       description="Описание API для проекта обмена объявлениями",
       contact=openapi.Contact(email="i.tkachenko@zohomail.eu"),
    ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('api/', include(router.urls)),
+
 ]
